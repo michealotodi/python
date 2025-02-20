69daaad90153cbf29d2eae84edce5decb9a7c05e
michealotodi <michealotodi81@gmail.com>
2025-02-20 21:40:37 +0300

    files added

---
+++ datatype.py
@@ -0,0 +1,22 @@
+#Data type specify the type of data that can be stored in side a variable
+
+#data type in python include:
+#numeric(int,float,complex)
+#string
+#mapping
+#boolean
+#set
+#sequence(list,tuple,range)
+num = 200 #an integers value
+num2 = 200.0 #float value
+print(type(num))
+print(type(num2))
+num3 = 1 + 2j #complex value
+print(type(num3))
+num4 = "5" #string
+
+word = "hello,world" #string 
+print(type(word))
+print(type(num4))
+print(type(word[1]))
+
---
+++ example2.py
@@ -0,0 +1,21 @@
+#a name given to a partition through programming is called variable
+num1 =65 #num1 is a variable
+num2 =100 #num2 is a variable
+print(num1)
+print(num1 + num2)
+#the name of the variable should not start with a numeral or a number eg 1num or 2num
+#the variable name should not start with a special character eg @num,$num,!num
+#composite variable name should not have spaces eg num 1,num 2
+#compound variable name should be joined by either a underscore or a camel notation eg my num_1,num_2,Num1
+#avoid using capital letters to begin the variable name eg Num1,Num2
+#do not use special words as variable name eg print,input for etc
+#variable in python are case sensitive eg Num1 is different from num1
+#those are the syntax about variable names in python as seen above
+#a complete instruction to a computer is called a statement
+#any line starting with a # is called a statement
+#a statement that evaulate to a result is called an expreession eg line5
+#a python file is called a python script
+#a group of statement is called a code
+#a group of related code is called a block
+#a group of block of code in a python script is called a program
+#the running or implementing of a program is called execution
