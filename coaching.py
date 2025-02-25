a4e0dff6c5f0106766fbf16e000e4f67d29408f7
sarah <angwensarahsunday@gmail.com>
2025-02-25 18:16:35 +0300

    file added

---
+++ coaching.py
@@ -0,0 +1,9 @@
+things = [100, "hello", 300.0, True, [1, 2, 3]]
+print(things[0]) #positio of 1000
+print(things[1]) #position of "hello"
+print(things[2]) #position of 300.0
+print(things[3]) #position of true
+print(things[4]) #position of [1,2,3]
+print(things[4][0]) #position of 1
+print(things[4][1]) #position of 2
+print(things[4][2]) #position of 3
--- datatype3.py
+++ datatype3.py
@@ -1,10 +1,10 @@
 #mapping data type
 student_detail = {"name":"mike","track":"python","gender":"male","age":23}
-print(student_detail["name"])
-print(student_detail.keys())
-print(student_detail.values())
+print(student_detail["name"]) #finding the name from the file
+print(student_detail.keys())  #finding the keys eg name,truck,gender,age
+print(student_detail.values())  #finding the values eg mike,python,male,23
 
 #set data type
 student_id={123,245,678,123,245,678}
-print(student_id)
+print(student_id) #the repeated vaues
 
